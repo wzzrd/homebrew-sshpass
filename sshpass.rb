c4935d7fc91e6a98498eff5f935e95fac013d3e8
Maxim Burgerhout <maxim@wzzrd.com>
2025-11-27 13:25:03 +0100

    Update sshpass.rb
    update to 1.10

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
-  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
+  sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
 
   def install
     system "./configure", "--disable-debug",
